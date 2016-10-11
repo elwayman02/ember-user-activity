b9d7ddab0f4009d69b3a961d3845fc2c88ed7f8f
Ryan Mark <rgmark@gmail.com>
2016-10-11 11:43:51 -0700

    fix weird indenting.

--- addon/mixins/fastboot-compat.js
+++ addon/mixins/fastboot-compat.js
@@ -3,10 +3,10 @@ import getOwner from 'ember-getowner-polyfill';
 import computed from 'ember-computed';
 
 export default Mixin.create({
-	_fastboot: computed(function() {
+    _fastboot: computed(function() {
       let owner = getOwner(this);
       return owner.lookup('service:fastboot');
     }),
-	
+
     _isFastBoot: computed.readOnly('_fastboot.isFastBoot')
 });
