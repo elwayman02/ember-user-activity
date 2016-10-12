53ff20218105c26d218e27f2bb92457890680453
Ryan Mark <rgmark@gmail.com>
2016-10-11 17:52:06 -0700

    add test for _isFastBoot computed property

--- tests/unit/mixins/fastboot-compat-test.js
+++ tests/unit/mixins/fastboot-compat-test.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 import FastbootCompatMixin from 'ember-user-activity/mixins/fastboot-compat';
-import { module, test } from 'qunit';
+import { moduleFor, test } from 'ember-qunit';
+const { getOwner } = Ember;
 
-module('Unit | Mixin | fastboot compat');
+moduleFor('mixin:fastboot-compat', 'Unit | Mixin | fastboot compat', {
+integration: true,
+  subject() {
+    let FastbootCompatObject = Ember.Object.extend(FastbootCompatMixin);
+    this.register('test-container:fastboot-compat-object', FastbootCompatObject);
+    return getOwner(this).lookup('test-container:fastboot-compat-object');
+  }
+});
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let FastbootCompatObject = Ember.Object.extend(FastbootCompatMixin);
-  let subject = FastbootCompatObject.create();
-  assert.ok(subject);
+test('isFastBoot in the browser', function(assert) {
+  const subject = this.subject();
+  assert.equal(subject.get('_isFastBoot'), undefined, `it should be undefined`);
 });
