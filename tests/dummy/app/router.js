217380c1e13e8e5c00a18b23b644ab42e8b83f52
Jordan Hawker <hawker.jordan@gmail.com>
2017-12-21 15:16:51 -0800

    Update Ember 2.17 via `ember init`

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,12 @@
-import Router from 'ember-router';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const AppRouter = Router.extend({
-  location: config.locationType
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
-AppRouter.map(function () {
+Router.map(function() {
 });
 
-export default AppRouter;
+export default Router;
