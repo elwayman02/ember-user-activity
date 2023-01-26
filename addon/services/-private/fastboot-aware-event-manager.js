8435974ee234c7895a3305e6b6cf5d4d5f504baa
Andrey Fel <andrey.fel@retailnext.net>
2023-01-26 21:57:01 +0100

    Simplify isFastBoot check
    I saw this technique in ember-data repo as a modern trick to detect if we
    are in a FastBoot env.

--- addon/services/-private/fastboot-aware-event-manager.js
+++ addon/services/-private/fastboot-aware-event-manager.js
@@ -1,16 +1,7 @@
-import { computed } from '@ember/object';
 import EventManagerService from 'ember-user-activity/services/-private/event-manager';
-import { getOwner } from '@ember/application';
-import { readOnly } from '@ember/object/computed';
 
 export default class FastBootAwareEventManagerService extends EventManagerService {
-  // Fastboot Compatibility
-  @computed
-  get _fastboot() {
-    let owner = getOwner(this);
-    return owner.lookup('service:fastboot');
+  get _isFastBoot() {
+    return typeof FastBoot !== 'undefined';
   }
-
-  @readOnly('_fastboot.isFastBoot')
-  _isFastBoot;
 }
