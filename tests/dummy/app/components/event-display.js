514e9f3de8c92628701b483421d2c56c8599a53f
Andrey Fel <andrey.fel@retailnext.net>
2023-01-26 21:54:30 +0100

    Glimmer components

--- tests/dummy/app/components/event-display.js
+++ tests/dummy/app/components/event-display.js
@@ -1,34 +1,25 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { inject as injectService } from '@ember/service';
 import { A } from '@ember/array';
-import { action } from '@ember/object';
-import { classNames } from '@ember-decorators/component';
 
-@classNames('eventDisplay')
 export default class EventDisplay extends Component {
-  eventName = 'userActive';
   events = A();
 
   @injectService('ember-user-activity@user-activity')
   userActivity;
 
-  didInsertElement() {
-    super.didInsertElement(...arguments);
-    this.userActivity.on(this.eventName, this, this.registerActivity);
+  constructor() {
+    super(...arguments);
+    this.userActivity.on(this.args.eventName, this, this.registerActivity);
   }
 
-  willDestroyElement() {
-    super.willDestroyElement(...arguments);
-    this.userActivity.off(this.eventName, this, this.registerActivity);
-    this.userActivity.disableEvent(this.eventName);
+  willDestroy() {
+    super.willDestroy();
+    this.userActivity.off(this.args.eventName, this, this.registerActivity);
+    this.userActivity.disableEvent(this.args.eventName);
   }
 
   registerActivity(event) {
     this.events.unshiftObject(event.type);
-  }
-
-  @action
-  closeDisplay() {
-    this.close(this.eventName);
   }
 }
--- tests/dummy/app/components/idle-display.js
+++ tests/dummy/app/components/idle-display.js
@@ -1,19 +1,11 @@
-import { readOnly } from '@ember/object/computed';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
 import { inject as injectService } from '@ember/service';
-import { classNames } from '@ember-decorators/component';
 
-@classNames('idleDisplay')
 export default class IdleDisplay extends Component {
   @injectService('user-idle') // use the dummy's version, don't pull from addon directly
   userIdle;
 
-  @readOnly('userIdle.isIdle')
-  isIdle;
-
-  @computed('isIdle')
   get status() {
-    return this.isIdle ? 'idle' : 'not idle';
+    return this.userIdle.isIdle ? 'idle' : 'not idle';
   }
 }
